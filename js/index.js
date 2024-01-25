44858c3c78b58d32e6072a9920966527c91cded0
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2024-01-25 22:56:41 +0800

    113.01.25 調整專案(未完成)

--- js/index.js
+++ js/index.js
@@ -7,14 +7,15 @@ var hlogo = document.querySelector("#hlogo");
 
 
 window.addEventListener('scroll', function (params) {
-    if (html.scrollTop > 250) {
-        searchBar.classList.add('fixed');
-        hlogo.classList.add('hide')
-    } else {
-        searchBar.classList.remove('fixed');
-        hlogo.classList.remove('hide')
-    }
+    // if (html.scrollTop > 250) {
+    //     searchBar.classList.add('fixed');
+    //     hlogo.classList.add('hide')
+    // } else {
+    //     searchBar.classList.remove('fixed');
+    //     hlogo.classList.remove('hide')
+    // }
 });
+
 var searchHandler = document.querySelector("#searchHandler");
 searchHandler.onclick = function (params) {
     location.href = "result.html"
